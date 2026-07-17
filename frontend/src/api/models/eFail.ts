9263d073792515d22b1ca2315f227d585e50c22a
yellowjuice <leo.ziqi@gmail.com>
2026-07-17 02:17:15 +0000

    regenerate frontend api

--- frontend/src/api/models/eFail.ts
+++ frontend/src/api/models/eFail.ts
@@ -5,6 +5,7 @@
  * OpenAPI spec version: 0.1.0
  */
 
-export type CreateAutolabelsAutoLabelsPostParams = {
-requestKey?: string | null;
-};
+export const EFailValue = {
+  version: 'efail',
+} as const;
+export type EFail = typeof EFailValue;
